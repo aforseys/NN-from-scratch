2f43e6c8aec3f87e51a037d5b2cadf636de38d9a
Alexandra Forsey <aforsey@dhcp-10-29-61-11.dyn.MIT.EDU>
2023-05-01 14:13:17 -0400

    Backprop test against finite differences

--- init_NN.py
+++ init_NN.py
@@ -9,7 +9,7 @@ import numpy as np
 from activation_functions import softmax
 
 class NeuralNetwork:
-    def __init__(self,lsize,activation,dactivation, loss):
+    def __init__(self,lsize,activation,dactivation,loss, dloss):
         """
         Parameters
         ----------
@@ -25,6 +25,7 @@ class NeuralNetwork:
         self.act = activation
         self.dact = dactivation
         self.loss = loss
+        self.dloss = dloss
         self.init_params()
 
     def init_params(self):
@@ -40,35 +41,108 @@ class NeuralNetwork:
         #what is best way to initialize NN params?
         self.params ={}
         for l,lsize in enumerate(self.layers[:-1]):  #don't select parameters for output
-            A = np.random.randn(self.layers[l+1], lsize)
-            b = np.random.randn(self.layers[l+1], 1)
+            A = np.random.randn(self.layers[l+1], lsize) #A is sized mxn where n is height of current layer and m = height of next layer 
+            b = np.random.randn(self.layers[l+1], 1) #b is sized nx1 
             self.params[l] = (A,b)
             
-    def feedforward(self, x,y):
-        v = x
+    def feedforward(self, x,y, print_flag=False):
+        vecs = []
+        v = x.reshape(np.shape(x)[0],1)
+        vecs.append(v)
         for k in range(len(self.layers[:-1])): #double check appropriate indexing for architecture 
-            A,b = self.parms[k]
+            A,b = self.params[k]
+            
             v = np.dot(A,v)+b 
             v = self.act(v)
-        
+        #    print('after act', np.shape(v))
+            vecs.append(v)
        # A,b = self.params[k+1]
        # v = np.dot(A,v)+b
-        v=softmax(v)
-            
-        return v
-            
-    #def backprop(self,batch):
-     #   for x,y in batch:
-      #      v = self.feedforward(x,y)
-            #calculate gradients
-        #take average of gradients
-        #take step 
-
-    #def train_minibatch(self,num):
-        #break data up by input number 
-        #repeat backprop for all groups
+      #  vecs[-1]=softmax(vecs[-1]) #change output to softmax of output #don't do this here, bcus need just z later
         
-    #could implement training different ways (like GC or SGC)
+      #  print('output z', v)
+        error = self.loss(y, softmax(v), print_flag)
+        
+      #  print('error',error)
+        
+        return vecs, error
+            
+    def backprop(self,X,Y, alpha=0.001):
+        grad_bs = {l:[] for l in range(len(self.layers)-1)}
+        grad_As = {l:[] for l in range(len(self.layers)-1)} 
+        
+        for i in range(X.shape[0]):
+        #    x = np.transpose(X.reshape(1, np.shape(X)[0]))
+        #    y = np.transpose(Y.reshape(1, np.shape(Y)[0]))
+            x = np.transpose(X[i].reshape(1,np.size(X[i])))
+            y = np.transpose(Y[i].reshape(1,np.size(Y[i])))
+            vecs, error = self.feedforward(x,y)
+            z = vecs[-1]
+     #       print("output z from end of vecs", z)
+            W_T = np.transpose(self.dloss(y,z))    #initial W that is used to calculate all other W's is just dL/dz
+            
+            #print(W_T)
+            
+            for l, lsize in reversed(list(enumerate(self.layers[:-1]))): #go from last layer to first layer 
+              #  print("W_T", W_T)
+              #  print("l")
+                A,b = self.params[l]
+                a = A.dot(vecs[l])+b
+                
+               # print("dReLU/da shape", np.shape(self.dact(a)))
+                
+              #  print("W^T shape", np.shape(W_T))
+        
+                #calculate gradients (don't actually have to transpose ReLU deriv)
+                grad_A = np.outer(np.transpose(self.dact(a)).dot(np.transpose(W_T)), np.transpose(vecs[l])) #FIX THIS SO THAT IT'S ELEMENTWISE PRODUCT (nevermind, only when mult da)
+                grad_b = W_T.dot(self.dact(a)) #output is row vector => what is multiplied by delta b (a col vector) to get scalar df
+                
+                #store
+                grad_As[l].append(grad_A)
+                grad_bs[l].append(np.transpose(grad_b))
+                
+                #FIX THIS SO THAT TAKE MEAN OF THESE AS GO ALONG, DON'T STORE! BIG MATRICES AND A LOT OF THEM! 
+                
+                W_T = W_T.dot(self.dact(a)).dot(A) #calc W using old W (make sure that W^T correct..) 
+                
+       # return grad_bs, grad_As
+        #return grad_As, grad_bs
+        #TO DO: CHECK/DEBUG BY COMPARING BACK PROP W FINITE DIFFERENCE 
     
+        #Find average of gradients by taking average across each list in each layer of both dictionaries 
+        A_grad_descent_step = {l: np.mean(grad_As[l],axis=0)for l in range(len(self.layers)-1) }
+        b_grad_descent_step = {l: np.mean(grad_bs[l],axis=0)for l in range(len(self.layers)-1) }
         
-
+        #Gradient descent: Update self.params by taking those steps!!
+        for l in range(len(self.layers)-1):
+            A,b = self.params[l] 
+       #     print('delta A', -alpha*grad_As[l][0])
+        #    print('delat B', -grad_bs[l][0])
+            self.params[l] = (A-alpha*A_grad_descent_step[l][0], b - alpha*b_grad_descent_step[l][0]) #right now takes every step it finds 
+            
+    def train_minibatch(self, X, Y, batch_size):
+        #break up data into batches 
+        X_batched = [X[i:i + batch_size,:] for i in range(0, len(X), batch_size)]
+        Y_batched = [Y[i:i + batch_size,:] for i in range(0, len(Y), batch_size)]
+        
+        for i in range(len(X_batched)):
+            self.backprop(X_batched[i],Y_batched[i])
+            
+    def train_epoch(self, X,Y, epochs,batch_size):
+        for i in range(epochs):
+            self.train_minibatch(X,Y,batch_size)
+        
+    #def train_GD(self, num):
+    
+    #def train_SGD(self, num):
+        
+    def test(self,X,Y):
+        errors = []
+        for i in range(np.shape(X)[0]):
+            vecs, error = self.feedforward(np.transpose(X[i].reshape(1,np.size(X[i]))), np.transpose(Y[i].reshape(1,np.size(Y[i]))))
+            errors.append(error)
+        print(errors)
+        print(np.mean(errors))
+        return errors
+            
+        
